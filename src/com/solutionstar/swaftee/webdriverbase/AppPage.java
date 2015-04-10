b06aa8626b6073363b231ce9e501bd0b7c99884c
Allen Godfrey <allen@solutionstar.com>
2015-04-10 17:30:14 +0530

    Committing after Shelton's comments

--- src/com/solutionstar/swaftee/webdriverbase/AppPage.java
+++ src/com/solutionstar/swaftee/webdriverbase/AppPage.java
@@ -740,32 +740,7 @@ public class AppPage extends TestListenerAdapter
    
    public void uploadFile(WebElement element, String fileName)
    {
-	   element.click();
-	   sleep(1000);
-	   /*Actions action=new Actions(this.driver);
-	   
-	   action.sendKeys(fileName).build().perform();
-	   action.sendKeys(Keys.ENTER);*/
-	   try 
-	   {
-		   //Setting clipboard with file location
-           setClipboardData(fileName);
-           
-           //native key strokes for CTRL, V and ENTER keys
-           Robot robot = new Robot();
-	
-           robot.keyPress(KeyEvent.VK_CONTROL);
-           robot.keyPress(KeyEvent.VK_V);
-           robot.keyRelease(KeyEvent.VK_V);
-           robot.keyRelease(KeyEvent.VK_CONTROL);
-           robot.keyPress(KeyEvent.VK_ENTER);
-           robot.keyRelease(KeyEvent.VK_ENTER);
-       }
-	   catch (Exception exp) 
-	   {
-		   exp.printStackTrace();
-       }
-	   sleep(1000);
+	  element.sendKeys(fileName);
    }
    
    /**
