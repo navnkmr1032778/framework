bb986270cc07fe228810b43d441db59b5ef8dd2d
Goutham S <gouthamraj@solutionstar.com>
2015-08-17 19:02:55 +0530

    zephyr api call reduction

---
+++ src/com/solutionstar/swaftee/jira/Zephyr.java
@@ -0,0 +1,31 @@
+package com.solutionstar.swaftee.jira;
+
+public class Zephyr
+{
+	public static String testCycleId = System.getProperty("testCycleId");
+	
+	public static void resetTestCycle()
+	{
+		ZephyrUtils.initZephyr(testCycleId);
+		ZephyrUtils.resetExecutionStatusForCycle();
+	}
+	
+	public static void generateHtmlReport()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		String mode = System.getProperty("zephyr");
+		if(mode.equals("reset"))
+		{
+			resetTestCycle();
+		}
+		else if(mode.equals("generate-report"))
+		{
+			generateHtmlReport();
+		}
+	}
+
+}
--- src/com/solutionstar/swaftee/jira/ZephyrUtils.java
+++ src/com/solutionstar/swaftee/jira/ZephyrUtils.java
@@ -1,17 +1,18 @@
 package com.solutionstar.swaftee.jira;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +20,7 @@ import com.google.gson.internal.LinkedTreeMap;
 import com.solutionstar.swaftee.utils.CommonProperties;
 import com.solutionstar.swaftee.utils.CommonUtils;
 
+@SuppressWarnings("unchecked")
 public class ZephyrUtils
 {
 	public final static String PASS = "1";
@@ -34,156 +36,211 @@ public class ZephyrUtils
 
 	static CommonUtils utils = new CommonUtils();
 
-	public ZephyrUtils()
+	public static void initZephyr(String testCycleId)
 	{
-		CommonProperties props = CommonProperties.getInstance();
-		cycleId = System.getProperty("testCycleId");
-		try
+		if (cycleId == null)
 		{
-			props.load("./conf/jiraconfiguration.properties");
-			jiraServer = props.get("jira_server");
-			jiraAuth = props.get("jira_auth_token");
+			CommonProperties props = CommonProperties.getInstance();
+			cycleId = testCycleId;
+			try
+			{
+				props.load("./conf/jiraconfiguration.properties");
+				jiraServer = props.get("jira_server");
+				jiraAuth = props.get("jira_auth_token");
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-
 	}
 
-	public void resetExecutionStatus()
+	public static HashMap<String, String> createNewTestExecution(
+			String[] testCases)
 	{
+		HashMap<String, String> testExectionIdMap = new HashMap<String, String>();
 
+		Client client = ClientBuilder.newClient();
+
+		/*
+		 * Get project id
+		 */
+		Response response = client
+				.target(jiraServer + "rest/zapi/latest/cycle/" + cycleId)
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth).get();
+
+		logger.info("Status : " + String.valueOf(response.getStatus()) + " - "
+				+ response.toString());
+		Map<String, Object> responseMap = utils.convertJSONToMap(response
+				.readEntity(String.class));
+		Object projectId = responseMap.get("projectId");
+
+		/*
+		 * Add test to cycle - request will return appropriate message if test
+		 * is already added to the cycle
+		 */
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put("issues", testCases);
+		hm.put("cycleId", cycleId);
+		hm.put("projectId",
+				String.valueOf(Double.valueOf(projectId.toString()).intValue()));
+		hm.put("methodId", String.valueOf(1));
+
+		response = client
+				.target(jiraServer
+						+ "rest/zapi/latest/execution/addTestsToCycle")
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
+				.post(Entity.json(utils.objToJson(hm)));
+
+		logger.info("Status : " + String.valueOf(response.getStatus()) + " - "
+				+ response.toString());
+
+		/*
+		 * Create & perform execution for all test cases
+		 */
+
+		for (String s : testCases)
+		{
+			/*
+			 * Get JIRA issue id
+			 */
+			response = client.target(jiraServer + "rest/api/latest/search")
+					.queryParam("jql", "%20id%20in%20(" + s + ")")
+					.request(MediaType.APPLICATION_JSON_TYPE)
+					.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
+					.get();
+
+			logger.info("Status : " + String.valueOf(response.getStatus())
+					+ " - " + response.toString());
+
+			responseMap = (HashMap<String, Object>) utils.constructObjfromJson(
+					response.readEntity(String.class), responseMap.getClass());
+			ArrayList<?> issues = (ArrayList<?>) responseMap.get("issues");
+			LinkedTreeMap<String, Object> ltm = (LinkedTreeMap<String, Object>) issues
+					.get(0);
+
+			/*
+			 * Create execution
+			 */
+			hm.clear();
+			hm.put("issueId", ltm.get("id").toString());
+			hm.put("cycleId", cycleId);
+			hm.put("projectId", String.valueOf(Double.valueOf(
+					projectId.toString()).intValue()));
+
+			response = client.target(jiraServer + "rest/zapi/latest/execution")
+					.request(MediaType.APPLICATION_JSON_TYPE)
+					.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
+					.post(Entity.json(utils.objToJson(hm)));
+
+			logger.info("Status : " + String.valueOf(response.getStatus())
+					+ " - " + response.toString());
+
+			responseMap = (HashMap<String, Object>) utils.constructObjfromJson(
+					response.readEntity(String.class), responseMap.getClass());
+
+			testExectionIdMap.put(s,
+					responseMap.keySet().toArray()[0].toString());
+		}
+		client.close();
+
+		return testExectionIdMap;
+
+	}
+	
+	public static Map<String, Object> getInfoFromTestCycle()
+	{
+		Client client = ClientBuilder.newClient();
+
+		Response response = client
+				.target(jiraServer + "rest/zapi/latest/execution")
+				.queryParam("cycleId", cycleId)
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth).get();
+
+		Map<String, Object> responseMap = utils.convertJSONToMap(response
+				.readEntity(String.class));
+		
+		client.close();
+		
+		return responseMap;
+	}
+
+	public static HashMap<String, String> getExecutionIdFromTestCycle()
+	{
+		HashMap<String, String> testExectionIdMap = new HashMap<String, String>();
+		
+		Map<String, Object> responseMap = getInfoFromTestCycle();
+
+		List<LinkedTreeMap<String, Object>> executions = (List<LinkedTreeMap<String, Object>>) responseMap
+				.get("executions");
+
+		for (LinkedTreeMap<String, Object> execution : executions)
+		{
+			testExectionIdMap.put(execution.get("issueKey").toString(), ""
+					+ ((Double) execution.get("id")).longValue());
+		}
+
+		return testExectionIdMap;
+	}
+
+	public static void bulkUpdateStatus(Collection<String> executionIds,
+			String status)
+	{
+		Client client = ClientBuilder.newClient();
+
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put("status", status);
+		hm.put("stepStatus", "1");
+		hm.put("executions",
+				executionIds.toArray(new String[executionIds.size()]));
+
+		String json = utils.objToJson(hm);
+
+		Response response = client
+				.target(jiraServer
+						+ "rest/zapi/latest/execution/updateBulkStatus")
+				.request(MediaType.APPLICATION_JSON_TYPE)
+				.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
+				.put(Entity.json(json));
+
+		logger.info("Status : " + String.valueOf(response.getStatus()) + " - "
+				+ response.toString());
+
+		client.close();
+	}
+
+	public static void resetExecutionStatusForCycle()
+	{
+		HashMap<String, String> testCaseExecutionMap = getExecutionIdFromTestCycle();
+		bulkUpdateStatus(testCaseExecutionMap.values(), UNEXECUTED);
 	}
 
 	public static void updateExecutionStatusInJIRA()
 	{
 		try
 		{
+			// group test cases as per status
 
-			String[] testCases = testCaseStatus.keySet().toArray(
-					new String[testCaseStatus.keySet().size()]);
-
-			if (testCases.length > 0)
+			HashMap<String, Collection<String>> groupingMap = new HashMap<String, Collection<String>>();
+			for (String testCase : testCaseStatus.keySet())
 			{
-				logger.info("Updating execution status for test cases "
-						+ StringUtils.join(testCases, ", ") + " in JIRA..");
-
-				Client client = ClientBuilder.newClient();
-
-				/*
-				 * Get project id
-				 */
-				Response response = client
-						.target(jiraServer + "rest/zapi/latest/cycle/"
-								+ cycleId)
-						.request(MediaType.APPLICATION_JSON_TYPE)
-						.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
-						.get();
-
-				logger.info("Status : " + String.valueOf(response.getStatus())
-						+ " - " + response.toString());
-				Map<String, Object> responseMap = utils
-						.convertJSONToMap(response.readEntity(String.class));
-				Object projectId = responseMap.get("projectId");
-
-				/*
-				 * Add test to cycle - request will return appropriate message
-				 * if test is already added to the cycle
-				 */
-				HashMap<String, Object> hm = new HashMap<String, Object>();
-				hm.put("issues", testCases);
-				hm.put("cycleId", cycleId);
-				hm.put("projectId", String.valueOf(Double.valueOf(
-						projectId.toString()).intValue()));
-				hm.put("methodId", String.valueOf(1));
-
-				response = client
-						.target(jiraServer
-								+ "rest/zapi/latest/execution/addTestsToCycle")
-						.request(MediaType.APPLICATION_JSON_TYPE)
-						.header(HttpHeaders.AUTHORIZATION, "Basic " + jiraAuth)
-						.post(Entity.json(utils.objToJson(hm)));
-
-				logger.info("Status : " + String.valueOf(response.getStatus())
-						+ " - " + response.toString());
-
-				/*
-				 * Create & perform execution for all test cases
-				 */
-
-				for (String s : testCases)
+				if (groupingMap.containsKey(testCaseStatus.get(testCase)))
 				{
-					/*
-					 * Get JIRA issue id
-					 */
-					response = client
-							.target(jiraServer + "rest/api/latest/search")
-							.queryParam("jql", "%20id%20in%20(" + s + ")")
-							.request(MediaType.APPLICATION_JSON_TYPE)
-							.header(HttpHeaders.AUTHORIZATION,
-									"Basic " + jiraAuth).get();
-
-					logger.info("Status : "
-							+ String.valueOf(response.getStatus()) + " - "
-							+ response.toString());
-
-					responseMap = (HashMap<String, Object>) utils
-							.constructObjfromJson(
-									response.readEntity(String.class),
-									responseMap.getClass());
-					ArrayList<?> issues = (ArrayList<?>) responseMap
-							.get("issues");
-					LinkedTreeMap<String, Object> ltm = (LinkedTreeMap<String, Object>) issues
-							.get(0);
-
-					/*
-					 * Create execution
-					 */
-					hm.clear();
-					hm.put("issueId", ltm.get("id").toString());
-					hm.put("cycleId", cycleId);
-					hm.put("projectId", String.valueOf(Double.valueOf(
-							projectId.toString()).intValue()));
-
-					response = client
-							.target(jiraServer + "rest/zapi/latest/execution")
-							.request(MediaType.APPLICATION_JSON_TYPE)
-							.header(HttpHeaders.AUTHORIZATION,
-									"Basic " + jiraAuth)
-							.post(Entity.json(utils.objToJson(hm)));
-
-					logger.info("Status : "
-							+ String.valueOf(response.getStatus()) + " - "
-							+ response.toString());
-
-					responseMap = (HashMap<String, Object>) utils
-							.constructObjfromJson(
-									response.readEntity(String.class),
-									responseMap.getClass());
-
-					hm.clear();
-					String status = testCaseStatus.get(s);
-					hm.put("status", status);
-
-					/*
-					 * Perform execution
-					 */
-					response = client
-							.target(jiraServer
-									+ "rest/zapi/latest/execution/"
-									+ responseMap.keySet().toArray()[0]
-											.toString() + "/quickExecute")
-							.request(MediaType.APPLICATION_JSON_TYPE)
-							.header(HttpHeaders.AUTHORIZATION,
-									"Basic " + jiraAuth)
-							.post(Entity.json(utils.objToJson(hm)));
-
-					logger.info("Status : "
-							+ String.valueOf(response.getStatus()) + " - "
-							+ response.toString());
+					groupingMap.get(testCaseStatus.get(testCase)).add(testCase);
 				}
-				client.close();
+				else
+				{
+					Collection<String> testCases = new ArrayList<String>();
+					testCases.add(testCase);
+					groupingMap.put(testCaseStatus.get(testCase), testCases);
+				}
+			}
+			for (String status : groupingMap.keySet())
+			{
+				bulkUpdateStatus(groupingMap.get(status), status);
 			}
 
 		}
@@ -195,6 +252,7 @@ public class ZephyrUtils
 	}
 
 	/**
+	 * Updates the test case status in a static variable
 	 * 
 	 * @param testCases
 	 *            - JIRA test cases whose execution statuses are to be updated
@@ -202,7 +260,7 @@ public class ZephyrUtils
 	 *            - Execution status
 	 * 
 	 */
-	public void updateExecutionStatusOfTests(String[] testCases, String status)
+	public static void updateExecutionStatusOfTests(String[] testCases, String status)
 	{
 		for (String testCase : testCases)
 		{
