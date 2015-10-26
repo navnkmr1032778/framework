070a7543d91b46c9352361bb61fdf76c5ad404e3
Goutham S <gouthamraj@solutionstar.com>
2015-10-26 13:33:26 +0530

    throw sftp exceptions so that they can be better handled by consumers

--- src/com/solutionstar/swaftee/utils/CommonUtils.java
+++ src/com/solutionstar/swaftee/utils/CommonUtils.java
@@ -38,6 +38,7 @@ import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 import com.solutionstar.swaftee.constants.WebDriverConstants;
 
 public class CommonUtils {
@@ -400,8 +401,8 @@ public class CommonUtils {
 
 	public void copyToSFTPLocation(String hostname, int port, String username,
 			String password, String sourceLocation, String destination,
-			List<String> files) {
-		try {
+			List<String> files) throws SftpException  {
+
 			channelSftp = getChannelSftp(hostname, port, username, password);
 			for (String file : files) {
 				String fileName = file.substring(file.lastIndexOf("/") + 1);
@@ -413,9 +414,6 @@ public class CommonUtils {
 				channelSftp.disconnect();
 			if (session != null)
 				session.disconnect();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
 	}
 
 	public List<String> listFilesInSFTPLocation(String hostname, int port,
