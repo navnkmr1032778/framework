e6008cc20236714612f6036991b23ad03ea96467
Goutham S <gouthamraj@solutionstar.com>
2015-11-23 18:12:57 +0530

    sample message to test the framework

--- src/com/solutionstar/swaftee/jira/Zephyr.java
+++ src/com/solutionstar/swaftee/jira/Zephyr.java
@@ -1,5 +1,8 @@
 package com.solutionstar.swaftee.jira;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Zephyr
 {
 	public static String testCycleId = System.getProperty("testCycleId");
@@ -12,7 +15,19 @@ public class Zephyr
 	
 	public static void generateHtmlReport()
 	{
-		
+		String writeToFile = System.getProperty("writeTo");
+		String output = "<table><tr><th>Jira Key</th><th>Name</th><th>Description</th><th>Result</th></tr>" +
+		"<tr><td>CAS-123</td><td>Test 1</td><td>Description of the test</td><td>Unexecuted</td></tr>";
+		try
+		{
+			FileWriter fw = new FileWriter(writeToFile);
+			fw.write(output);
+			fw.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args)
