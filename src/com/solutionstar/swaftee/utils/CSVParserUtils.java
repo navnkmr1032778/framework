ecd016cd3047acee3a0c3e951e6bd916b0402774
Saran Prasad Ambikapathy <saran.ambikapathy@corp.xome.com>
2016-08-30 11:54:31 +0530

    wrote a append version appendListHashMapToCSV() of writeListHashMapToCSV()

--- src/com/solutionstar/swaftee/utils/CSVParserUtils.java
+++ src/com/solutionstar/swaftee/utils/CSVParserUtils.java
@@ -46,7 +46,7 @@ public class CSVParserUtils {
 
 			CSVReader reader = new CSVReader(new FileReader(
 					utils.getCurrentWorkingDirectory()
-							+ WebDriverConstants.PATH_TO_TEST_DATA_FILE
+							+ WebDriverConstants.WINDOWS_PATH_TO_TEST_DATA_DIR
 							+ fileName + ".csv"));
 			List<String[]> rowEntries = reader.readAll();
 			for (String[] row : rowEntries) {
@@ -456,6 +456,38 @@ public class CSVParserUtils {
 			logger.error("Error in writeListHashMapToCSV() - " + ExceptionUtils.getFullStackTrace(e)); 
 		}
 	}
+	
+	public void appendListHashMapToCSV(String fileName, List<HashMap<String,String>> data, String[] header) {
+		try {
+			List<String[]> dataToCSV = new ArrayList<String[]>();
+			int count = header.length;
+			String[] cells = new String[count];
+			int index = 0;	
+			
+			//Create headers only the first time
+			if(!(new File(fileName).exists())){
+				for(String heading : header) {
+					cells[index] = heading;
+					index++;
+				}
+				dataToCSV.add(cells);
+			}
+			
+			for(HashMap<String,String> map : data) {
+				cells = new String[count];
+				index = 0;
+				for(String heading : header) {
+					cells[index] = map.get(heading);
+					index++;
+				}
+				dataToCSV.add(cells);
+			}
+			appendToCSVFile(fileName, dataToCSV);
+		} catch (Exception e) {
+			logger.error("Error in appendListHashMapToCSV() - " + ExceptionUtils.getFullStackTrace(e)); 
+		}
+	}
+	
 
 	public HashMap<String, List<HashMap<String, String>>> getHashfromAllFiles(File[] directoryListing,String index,String index2,Boolean includeFileName) throws Exception 
 	{
