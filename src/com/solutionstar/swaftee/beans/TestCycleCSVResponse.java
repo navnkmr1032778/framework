fded800866ed538eb306065d1ea28a5b231f4039
Praveen Pandian <Praveen.Pandian@xome.com>
2017-02-17 12:18:34 +0530

    Added test cycle report download utility method to ZephyrUtils.java

---
+++ src/com/solutionstar/swaftee/beans/TestCycleCSVResponse.java
@@ -0,0 +1,11 @@
+package com.solutionstar.swaftee.beans;
+
+// TestCycle Response POJO
+public class TestCycleCSVResponse {
+	
+	private String url;
+
+	public String getUrl() {
+		return url;
+	}
+}
---
+++ src/com/solutionstar/swaftee/constants/ZephyrConstants.java
@@ -0,0 +1,8 @@
+package com.solutionstar.swaftee.constants;
+
+public class ZephyrConstants {
+
+	public static final String CAPTURE_CSV_DATE = "dd_MM_yyyy_HH-mm-ss";
+	public static final String TESTCYCLE_URL ="https://jira.solutionstarit.com/rest/zapi/1.0/cycle/";
+	public static final String RESOURCE_FOLDER = "resources/testCycleReport/";
+}
