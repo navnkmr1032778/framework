0a88e30be8b4adfa299e3ea8885a8127a65b27b5
Dinesh J <dineshkumar.j@xome.com>
2017-07-13 14:12:25 +0530

    Added getRequest() with request parameter

--- src/com/solutionstar/swaftee/restassured/apicalls/APICalls.java
+++ src/com/solutionstar/swaftee/restassured/apicalls/APICalls.java
@@ -2,6 +2,8 @@ package com.solutionstar.swaftee.restassured.apicalls;
 
 import static com.jayway.restassured.RestAssured.given;
 
+import java.util.HashMap;
+
 import com.jayway.restassured.response.Cookie;
 import com.jayway.restassured.response.Response;
 import com.solutionstar.swaftee.restassured.*;
@@ -12,16 +14,16 @@ public class APICalls {
 	Utils utils = new Utils();
 
 	public Response getRequest(String endpoint) {
-
 		return given().headers(utils.getHeaders()).when().get(endpoint).thenReturn();
-
+	}
+	
+	public Response getRequest(String endpoint, HashMap<String, String> param) {
+		return given().parameters(param).headers(utils.getHeaders1()).when().get(endpoint).thenReturn();
 	}
 
 	public Response getRequest(Response res, String endpoint) {
-
 		return given().cookie(utils.getDetailedCookieUtil(res, APIConstants.RW_API_AUTH)).headers(utils.getHeaders1())
 				.when().get(endpoint).thenReturn();
-
 	}
 
 	public Response postRequest(Object body, String endpoint) {
